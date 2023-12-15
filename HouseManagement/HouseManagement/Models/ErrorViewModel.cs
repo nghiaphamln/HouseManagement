eedf8f86d2ebb0093a01e1c73d71f6afbfde7316
Minh Nghĩa <nghiaphamln3@gmail.com>
2023-12-15 23:55:53 +0700

    first commit

---
+++ HouseManagement/HouseManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HouseManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
