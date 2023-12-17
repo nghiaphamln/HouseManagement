be886ace3a9c22d404b4f8ca72d198f98f476db2
Minh Nghĩa <nghiaphamln3@gmail.com>
2023-12-17 22:28:06 +0700

    feat: new register form

---
+++ HouseManagement/HouseManagement/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseManagement.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
