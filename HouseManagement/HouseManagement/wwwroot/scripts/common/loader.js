5e49e19a1063b1a209ef63f72de2b8b4344df36e
Minh Nghĩa <nghiaphamln3@gmail.com>
2023-12-30 10:12:57 +0700

    feat: loader

---
+++ HouseManagement/HouseManagement/wwwroot/scripts/common/loader.js
@@ -0,0 +1,11 @@
+﻿angular.module('HouseManagement').service('LoaderService', function () {
+    this.IsLoading = true;
+
+    this.ShowLoader = function () {
+        this.isLoading = true;
+    };
+
+    this.HideLoader = function () {
+        this.isLoading = false;
+    };
+});
