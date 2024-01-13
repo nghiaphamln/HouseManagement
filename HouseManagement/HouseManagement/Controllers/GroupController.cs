fd16f25c4c3dd5b0c7c71baf173564eca16eb054
Minh Nghĩa <nghiaphamln3@gmail.com>
2024-01-14 00:28:17 +0700

    feat: change created user

--- HouseManagement/HouseManagement/Controllers/GroupController.cs
+++ HouseManagement/HouseManagement/Controllers/GroupController.cs
@@ -26,7 +26,7 @@ public class GroupController(
     public async Task<IActionResult> Create([FromBody] CreateGroupRequest request)
     {
         request.TrackId = _trackId;
-        request.CreatedUser = User.FindFirstValue(ClaimTypes.Name)!;
+        request.CreatedUser = User.FindFirstValue(ClaimTypes.Email)!;
         return await ExecuteFunctionWithTrackId(() => logicGroup.Create(request), _trackId);
     }
 }
