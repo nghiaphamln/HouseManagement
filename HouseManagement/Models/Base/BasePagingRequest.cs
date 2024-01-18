935cbf500522920123706d6344e364179e8769dc
Minh Nghĩa <nghiaphamln3@gmail.com>
2024-01-18 22:00:54 +0700

    feat: listing for group data

---
+++ HouseManagement/Models/Base/BasePagingRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.Base;
+
+[Serializable]
+public class BasePagingRequest
+{
+    public DateTime? FromDate { get; set; }
+    public DateTime? ToDate { get; set; }
+    public int PageSize { get; set; } = 5;
+    public int PageNumber { get; set; } = 1;
+}
---
+++ HouseManagement/Models/Base/BasePagingResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Models.Base;
+
+[Serializable]
+public class BasePagingResponse<T>
+{
+    public required T Data { get; set; }
+    public int TotalRecord { get; set; }
+}
